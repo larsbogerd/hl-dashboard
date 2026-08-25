3beb4da5dd4726d764421ed9d5b171c925647264
Lars Bogerd <larsbogerd@gmail.com>
2026-08-25 19:51:07 +0200

    feat: add env example & proxy api

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.SONARR_URL,
+          changeOrigin: true,
+          headers: { 'X-Api-Key': env.SONARR_API_KEY },
+        },
+      },
+    },
+  }
 })
